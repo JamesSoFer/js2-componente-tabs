ae9ea59aecf03aca5dd20873f1f26e9366c8a6d6
JamesSoFer <66284023+JamesSoFer@users.noreply.github.com>
2020-09-15 19:44:39 -0600

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,26 +1,30 @@
-// Primer cuatri - Trabajo en clase 
-
-function tabsclick(event){
-    event.preventDefault();
-    const id = event.currentTarget.getAttribute('href').substring(1);
-    console.log(id);
-    for (let i = 0; i < content.lenght; i++){
-        content[i].style.display = 'none';
-        if(content[i].getAttribute('id') == id){
-            content[i].style.display = 'block';
-        }
-    }
-}
-
 const links = document.querySelectorAll('.tabs-tablist-item');
+
 const content = document.querySelectorAll('.tabs-content-item');
 
 for(let i = 0; i < content.lenght; i++){
-    content[i].style.display = 'none';
+    content[i].classList.add('js-content-hidden');
 }
 
-content[0].style.display = 'block';
+content[0].classList.remove('js-content-hidden');
+
+links[0].classList.add('js-active');
 
 for(let i = 0; i < links.lenght; i++){
-    links[i].addEventListener('click', tabsclick);
+    links[i].addEventListener('click', (event) => {
+        console.log(event);
+        event.preventDefault();
+        const id = event.currentTarget.getAttribute('href').substring(1);
+        console.log(id);
+        for (let i = 0; i < content.lenght; i++) {
+            content[i].classList.add('js-content-hidden');
+            if(content[i].getAttribute('id') === id){
+                content[i].classList.remove('js-content-hidden');
+            }
+        }
+        for(let i = 0; i < links.length; i++){
+            links[i].classList.remove('js-active');
+        }
+        event.currentTarget.classList.add('js-active');
+    });
 }
